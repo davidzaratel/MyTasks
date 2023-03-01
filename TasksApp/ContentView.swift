0bafc76c0addc3a874174e1bb3d1c7e91a31a682
David Zarate Lopez <a01329785@itesm.mx>
2023-03-01 17:00:03 +0100

    Changing lists from VStack to List and implemented editing the list (delete and change the order)

--- TasksApp/ContentView.swift
+++ TasksApp/ContentView.swift
@@ -10,42 +10,39 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var viewModel: ViewModel = ViewModel()
+    @State var searchList = ""
     
     var body: some View {
         NavigationView {
             ZStack{
                 Color("BackgroundColor").ignoresSafeArea()
                 VStack {
-                    ScrollView{
-                        VStack(spacing: 25) {
-                            if viewModel.isLoading {
-                            Spacer()
-                            ProgressView().frame(width: 500, height: 500, alignment: .center).scaleEffect(2)
-                            Spacer()                            
-                            } else {
-                                ForEach(viewModel.lists.indices, id: \.self) { index in
+                    List {
+                        ForEach(viewModel.lists.indices, id: \.self) { index in
+                                NavigationLink {
+                                    TasksView(index: index)
+                                } label: {
                                     HStack{
-                                        NavigationLink {
-                                            TasksView(index: index)
-                                        } label: {
-                                            HStack{
-                                                Text(viewModel.lists[index].title).padding()
-                                                Spacer()
-                                                Text(String(viewModel.lists[index].tasks.count)).padding()
-                                            }
-                                            .frame(height: 70)
-                                            .frame(maxWidth: .infinity)
-                                            .background(Color(viewModel.lists[index].color.title))
-                                            .foregroundColor(Color.white)
-                                            .font(.system(size: 20))
-                                            .cornerRadius(20)
-                                            .bold()
-                                        }.padding(.horizontal)
+                                        Text(viewModel.lists[index].title).padding()
+                                        Spacer()
+                                        Text(String(viewModel.lists[index].tasks.count)).padding()
                                     }
                                 }
-                            }
-                        }
-                    }.padding(.vertical, 30)
+                                    .padding(.horizontal)
+                                    .frame(height: 60)
+                                    .frame(maxWidth: .infinity)
+                                    .listRowBackground(Color(viewModel.lists[index].color.title))
+                                    .foregroundColor(Color.white)
+                                    .font(.system(size: 20))
+                                    .cornerRadius(20)
+                                    .bold()
+                            }                                    .onDelete(perform: delete)
+                                .onMove(perform: move)
+                    }
+                    .padding(.top)
+                    .scrollContentBackground(.hidden)
+//                    .searchable(text: $searchList)
+                    .navigationBarItems( trailing: EditButton())
                     Spacer()
                     BottomView()
                 }
@@ -53,6 +50,14 @@ struct ContentView: View {
             .navigationTitle("My Tasks")
         }.environment(\.colorScheme, .dark)
             .environmentObject(viewModel)
+    }
+    
+    func delete(indexSet: IndexSet) {
+        viewModel.deleteList(indexSet: indexSet)
+    }
+
+    func move(indices: IndexSet, newOffset: Int){
+        viewModel.moveListOrder(indices: indices, newOffset: newOffset)
     }
 }
 
